66ef5baed59deac8ff294b44fb4110675e8e5d12
Felix Neumann <dev@fxnn.de>
2024-04-25 00:08:02 +0200

    add tests for HtmlTextConverter

--- Java/TextConverter/src/test/java/tddmicroexercises/textconvertor/HtmlTextConverterTest.java
+++ Java/TextConverter/src/test/java/tddmicroexercises/textconvertor/HtmlTextConverterTest.java
@@ -6,10 +6,27 @@ import org.junit.jupiter.api.Test;
 
 public class HtmlTextConverterTest {
 
+  public static final String TXT_MULTIPLE_LINES = "src/test/resources/multipleLines.txt";
+  public static final String TXT_ESCAPING = "src/test/resources/escaping.txt";
+
   @Test
-  public void foo() {
-    HtmlTextConverter converter = new HtmlTextConverter("foo");
-    assertEquals("fixme", converter.getFilename());
+  public void getFilename() {
+    HtmlTextConverter converter = new HtmlTextConverter(TXT_MULTIPLE_LINES);
+    assertEquals(TXT_MULTIPLE_LINES, converter.getFilename());
+  }
+
+  @Test
+  public void convertToHtml_multipleLines() throws Exception {
+    HtmlTextConverter converter = new HtmlTextConverter(TXT_MULTIPLE_LINES);
+    String expected = "line 1<br />line 2<br />line 3<br />";
+    assertEquals(expected, converter.convertToHtml());
+  }
+
+  @Test
+  public void convertToHtml_escaping() throws Exception {
+    HtmlTextConverter converter = new HtmlTextConverter(TXT_ESCAPING);
+    String expected = "&lt;&quot; that&quot;s a test &amp; a win! &quot;&gt;<br />";
+    assertEquals(expected, converter.convertToHtml());
   }
 
 }
