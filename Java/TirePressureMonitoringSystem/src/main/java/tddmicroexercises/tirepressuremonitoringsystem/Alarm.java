e47a43a4d5aef26c4bbc4824ee074b84bbb4ffd2
Felix Neumann <dev@fxnn.de>
2024-04-24 23:41:26 +0200

    solve TirePressureMonitoringSystem

--- Java/TirePressureMonitoringSystem/src/main/java/tddmicroexercises/tirepressuremonitoringsystem/Alarm.java
+++ Java/TirePressureMonitoringSystem/src/main/java/tddmicroexercises/tirepressuremonitoringsystem/Alarm.java
@@ -1,26 +1,30 @@
 package tddmicroexercises.tirepressuremonitoringsystem;
 
-public class Alarm
-{
-    private static final double LOW_PRESSURE_THRESHOLD = 17;
-    private static final double HIGH_PRESSURE_THRESHOLD = 21;
+public class Alarm {
+  private static final double LOW_PRESSURE_THRESHOLD = 17;
+  private static final double HIGH_PRESSURE_THRESHOLD = 21;
 
-    protected Sensor sensor = new Sensor();
+  protected Sensor sensor;
 
-    protected boolean alarmOn = false;
+  protected boolean alarmOn = false;
 
-    public void check()
-    {
-        double psiPressureValue = sensor.popNextPressurePsiValue();
+  public Alarm() {
+    this(new Sensor());
+  }
 
-        if (psiPressureValue < LOW_PRESSURE_THRESHOLD || HIGH_PRESSURE_THRESHOLD < psiPressureValue)
-        {
-            alarmOn = true;
-        }
+  Alarm(Sensor sensor) {
+    this.sensor = sensor;
+  }
+
+  public void check() {
+    double psiPressureValue = sensor.popNextPressurePsiValue();
+
+    if (psiPressureValue < LOW_PRESSURE_THRESHOLD || HIGH_PRESSURE_THRESHOLD < psiPressureValue) {
+      alarmOn = true;
     }
+  }
 
-    public boolean isAlarmOn()
-    {
-        return alarmOn; 
-    }
+  public boolean isAlarmOn() {
+    return alarmOn;
+  }
 }
--- Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
+++ Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
@@ -1,14 +1,57 @@
 package tddmicroexercises.tirepressuremonitoringsystem;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AlarmTest {
 
-    @Test
-    public void foo() {
-        Alarm alarm = new Alarm();
-        assertFalse(alarm.isAlarmOn());
+  @Test
+  public void beforeCheck__alarmIsOff() {
+    Alarm alarm = new Alarm(new Sensor());
+    assertFalse(alarm.isAlarmOn());
+  }
+
+  @Test
+  public void afterSomeChecks__alarmWasOn() {
+    Alarm alarm = new Alarm(new Sensor());
+    for (int i = 0; i < 100; i++) {
+      alarm.check();
+      if (alarm.isAlarmOn()) {
+        return;
+      }
     }
+    fail("Alarm was never on");
+  }
+
+  @Test
+  public void sensorReportsLowPressure__alarmIsOn() {
+    Alarm alarm = new Alarm(mockSensor(16));
+    alarm.check();
+    assertTrue(alarm.isAlarmOn());
+  }
+
+  @Test
+  public void sensorReportsHighPressure__alarmIsOn() {
+    Alarm alarm = new Alarm(mockSensor(22));
+    alarm.check();
+    assertTrue(alarm.isAlarmOn());
+  }
+
+  @Test
+  public void sensorReportsNormalPressure__alarmIsOff() {
+    Alarm alarm = new Alarm(mockSensor(18));
+    alarm.check();
+    assertFalse(alarm.isAlarmOn());
+  }
+
+  private static Sensor mockSensor(final int nextPressurePsiValue) {
+    return new Sensor() {
+      @Override
+      public double popNextPressurePsiValue() {
+        return nextPressurePsiValue;
+      }
+    };
+  }
+
 }
